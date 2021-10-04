ba118415c6ca7db94e732cab80c2a43a3a6ab6c0
Ednei R Louback <89962959+nei22@users.noreply.github.com>
2021-10-04 19:50:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let nome = 'rafael'
+let idade = 25
+let estaAprovado = true
+let sobreNome = undefined
+let corSelecionado = null
+
+let pessoa = {
+    nome: 'Rafael',
+    idade: 25,
+    estaAprovado: true,
+    sobreNome: 'de Souza'
+}
+console.log(pessoa)
